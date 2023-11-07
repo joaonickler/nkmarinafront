1448da0e8edcb820d92172e530a4c30d2126e0d1
Joao Victor da Silva Nickler <90014083+joaonickler@users.noreply.github.com>
2023-11-07 14:34:00 -0300

    Add files via upload
    Atualização

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
     exports: [
       MatCardModule,
       MatTableModule,
       MatToolbarModule,
-      MatProgressSpinnerModule
+      MatProgressSpinnerModule,
+      MatPaginatorModule
      ]
 })
 
 export class AppMaterialModule { }
+
