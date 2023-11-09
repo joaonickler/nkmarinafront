de46c7d702129fbaa6913ce4687117fd0514620d
Joao Victor da Silva Nickler <90014083+joaonickler@users.noreply.github.com>
2023-11-08 21:33:22 -0300

    Add files via upload
    Atualização

--- src/app/embarcacoes/embarcacoes-routing.module.ts
+++ src/app/embarcacoes/embarcacoes-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { EmbarcacoesComponent } from './embarcacoes/embarcacoes.component';
+import { EmbarcacoesFormComponent } from './embarcacoes-form/embarcacoes-form.component';
 
 const routes: Routes = [
-  {path :'', component: EmbarcacoesComponent}
+  {path :'', component: EmbarcacoesComponent},
+  {path :'new', component: EmbarcacoesFormComponent}
 ];
 
 @NgModule({
--- src/app/embarcacoes/embarcacoes.module.ts
+++ src/app/embarcacoes/embarcacoes.module.ts
@@ -1,19 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
+import { SharedModule } from '../shared/shared.module';
+import { EmbarcacoesFormComponent } from './embarcacoes-form/embarcacoes-form.component';
 import { EmbarcacoesRoutingModule } from './embarcacoes-routing.module';
 import { EmbarcacoesComponent } from './embarcacoes/embarcacoes.component';
 
 
 @NgModule({
   declarations: [
-    EmbarcacoesComponent
+    EmbarcacoesComponent,
+    EmbarcacoesFormComponent
   ],
   imports: [
     CommonModule,
     EmbarcacoesRoutingModule,
-    AppMaterialModule
+    AppMaterialModule,
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class EmbarcacoesModule { }
