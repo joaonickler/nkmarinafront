6cafb091247d890767db1c693db2d6e20dd1356a
Joao Victor da Silva Nickler <90014083+joaonickler@users.noreply.github.com>
2023-11-08 21:32:15 -0300

    Atualização

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -12,7 +17,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
       MatTableModule,
       MatToolbarModule,
       MatProgressSpinnerModule,
-      MatPaginatorModule
+      MatPaginatorModule,
+      MatButtonModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatSelectModule,
+      MatSnackBarModule
      ]
 })
 
