331dc6ea590493c199900cd4595fabc5f9449c98
Joao Victor da Silva Nickler <90014083+joaonickler@users.noreply.github.com>
2023-11-13 16:52:34 -0300

    Add files via upload
    Atualização

--- src/app/embarcacoes/services/embarcacoes.service.ts
+++ src/app/embarcacoes/services/embarcacoes.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Embarcacao } from '../model/embarcacao';
-import { first, tap } from 'rxjs';
+import {  first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -22,10 +22,41 @@ export class EmbarcacoesService {
     );
   }
 
-  save(record: Embarcacao){
-    return this.HttpClient.post<Embarcacao>(this.API,  record);
+  save(record: Partial<Embarcacao>)
+  {
+    console.log(record);
+    ///return this.HttpClient.post<Embarcacao>(this.API,  record);
+    if (record.id) {
+      console.log('ALTERACAO');
+      return this.update(record);
+    }
+      console.log('CRIACAO');
+      return this.create(record);
 
+  }
+
+  // Localiza por ID
+  loadById(id: String ){
+    return this.HttpClient.get<Embarcacao>(`${this.API}/${id}`);
+  }
+
+  // Cria um novo
+  private create(record: Partial<Embarcacao>){
+    return this.HttpClient.post<Embarcacao>(this.API,  record);
+  }
+
+  // Altera um existente
+  private update(record: Partial<Embarcacao>){
+     return this.HttpClient.post<Embarcacao>(this.API,  record);
+    }
+
+  private delete(id:String){
+    return this.HttpClient.delete<Embarcacao>(`${this.API}/${id}`);
   }
 
 
 }
+
+
+
+////return this.HttpClient.put<Embarcacao>(`${this.API}/${record.id}`,record);
