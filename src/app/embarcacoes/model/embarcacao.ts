62285411e8cbc207ed956e800d6cc9deb3d3768a
Joao Victor da Silva Nickler <90014083+joaonickler@users.noreply.github.com>
2023-11-13 16:52:59 -0300

    Add files via upload
    Atualização

--- src/app/embarcacoes/model/embarcacao.ts
+++ src/app/embarcacoes/model/embarcacao.ts
@@ -1,10 +1,8 @@
 export interface Embarcacao {
-    id  :   String;
-    nm_embarc : String;
-    nr_marina : String;
-    dh_registro : String;
-    id_situacao : String;
-    tipo: String;
+    id:String;
+    id_situacao:String;
+    nm_embarc:String;
+    tipo:String;
 
 
 }
