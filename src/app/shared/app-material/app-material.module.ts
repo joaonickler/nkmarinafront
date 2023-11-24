25bbcabe4a28954973560e58480f8717026d648b
Joao Victor da Silva Nickler <90014083+joaonickler@users.noreply.github.com>
2023-11-24 11:34:46 -0300

    Add files via upload
    Atualização

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule} from '@angular/material/tooltip';
 
 
 @NgModule({
@@ -22,7 +25,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
       MatFormFieldModule,
       MatInputModule,
       MatSelectModule,
-      MatSnackBarModule
+      MatSnackBarModule,
+      MatIconModule,
+      MatMenuModule,
+      MatTooltipModule,
      ]
 })
 
