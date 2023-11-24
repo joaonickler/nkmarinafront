30d74541e0cde74b055f9cc4c080edd72ba8126e
Joao Victor da Silva Nickler <90014083+joaonickler@users.noreply.github.com>
2023-11-24 11:56:22 -0300

    Atualização

--- src/app/agenda/agenda.module.ts
+++ src/app/agenda/agenda.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AgendaListComponent } from './components/agenda-list/agenda-list.component';
+import { AgendaComponent } from './containers/agenda/agenda/agenda.component';
+import { AgendaFormComponent } from './containers/agenda-form/agenda-form/agenda-form.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AgendaListComponent,
+    AgendaComponent,
+    AgendaFormComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ src/app/agenda/containers/agenda-form/agenda-form/agenda-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agenda-form',
+  templateUrl: './agenda-form.component.html',
+  styleUrls: ['./agenda-form.component.scss']
+})
+export class AgendaFormComponent {
+
+}
