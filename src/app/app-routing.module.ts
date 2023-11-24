41c0dd029e22aad84133439fe4a6ea664f662e60
Joao Victor da Silva Nickler <90014083+joaonickler@users.noreply.github.com>
2023-11-24 11:34:03 -0300

    Add files via upload
    Atualização

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
-  {  path: '', pathMatch: 'full',   redirectTo : 'embarcacoes'},
-  {  path: 'embarcacoes',
-     loadChildren: () => import('./embarcacoes/embarcacoes.module').then(m=>m.EmbarcacoesModule)
-  }
+  {  path: '', pathMatch: 'full',   redirectTo : 'home'},
+  {  path: 'clientes',loadChildren: () => import('./clientes/clientes.module').then(m=>m.ClientesModule) }
 ];
+
+
+///const routes: Routes = [
+  //{/  path: '', pathMatch: 'full',   redirectTo : 'clientes'},
+  ///{  path: 'clientes',loadChildren: () => import('./clientes/clientes.module').then(m=>m.ClientesModule) }
+///];
+
+///const routes: Routes = [
+//  {  path: '', pathMatch: 'full',   redirectTo : 'embarcacoes'},
+//  {  path: 'embarcacoes',
+     //loadChildren: () => import('./embarcacoes/embarcacoes.module').then(m=>m.EmbarcacoesModule)
+  //}
+//];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+
+
 })
 export class AppRoutingModule { }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,30 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    HttpClientModule
+    HttpClientModule,
+    MatMenuModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
